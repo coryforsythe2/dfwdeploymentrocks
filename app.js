c03766260e360b729d31c3b10544853631d9e195
coryforsythe2 <coryforsythe@Corys-iMac.local>
2016-06-07 22:34:43 -0500

    Xcode fail... just do hello world

--- app.js
+++ app.js
@@ -1,56 +1,10 @@
-var jid="592502_dfwdeploymentrocks-cforsythe2";
-var roomPassword="";
-var room="dfwdeploymentrocks-cforsythe2";
+var express = require('express');
+var app = express();
 
-
-var wobot = require('wobot');
-
-var b = new wobot.Bot({
-  jid: jid+'@chat.hipchat.com/bot',
-  password: roomPassword
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-b.connect();
-
-
-b.onConnect(function() {
-  console.log(' -=- > Connect');
-  this.join(room+'@conf.hipchat.com');
-
-  // fetch and print roster contacts (buddy list)
-  this.getRoster(function(err, items, stanza) {
-    if (err) {
-      console.log(' -=- > Error getting roster: ' + err);
-      return;
-    }
-    items.forEach(function(item) {
-      console.log(' -=- > Roster contact: ' + item.name);
-    });
-  });
-});
-
-b.onInvite(function(roomJid, fromJid, reason) {
-  console.log(' -=- > Invite to ' + roomJid + ' by ' + fromJid + ': ' + reason);
-  this.join(roomJid);
-});
-
-b.onPing(function() {
-  console.log(' -=- > Ping? Pong!');
-});
-
-b.onDisconnect(function() {
-  console.log(' -=- > Disconnect');
-});
-
-b.onError(function(error, text, stanza) {
-  console.log(' -=- > Error: ' + error + ' (' + text + ')');
-});
-
-b.onMessage(function(channel, from, message) {
-  console.log(' -=- > ' + from + '@' + channel + ' said: ' + message);
-  b.message(jid+'@conf.hipchat.com',"Oh really? is that important to you?");
-});
-
-b.onPrivateMessage(function(jid, message) {
-  console.log(' -=- > ' + jid + ' pm\'d: ' + message);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });

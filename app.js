849d8c493daa767507400320080e81c3a56abdce
coryforsythe2 <coryforsythe@Corys-iMac.local>
2016-06-07 23:03:05 -0500

    TG-4 #STATUS-In progress

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ app.get('/', function (req, res) {
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening!');
+  console.log('Example app listening OK!');
 });
